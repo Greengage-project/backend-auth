8de4bdc09efb2f1c00a2d74e231e08c3e1789c9e
fvergaracl <fvergaracl@gmail.com>
2024-05-08 09:47:49 +0200

    fix deleted unless variables

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ from typing import Any, Dict, List, Optional, Union
 from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
 import os
 
+
 class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
@@ -15,7 +16,7 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     MONGODB_DATABASE: str
     COLLECTION_NAME: str
-    
+
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
@@ -24,25 +25,24 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    CLIENT_ID: str
-    SERVER_URL: str
-    SERVER_METADATA_URL: str
-
-    
+    # CLIENT_ID: str
+    # SERVER_URL: str
+    # SERVER_METADATA_URL: str
 
     KEYCLOAK_CLIENT_ID: str
     KEYCLOAK_CLIENT_SECRET: str
     KEYCLOAK_REALM: str
     KEYCLOAK_URL_REALM: str
-    
 
     PROTOCOL: str
     SERVER_NAME: str
     BASE_PATH: str
-    COMPLETE_SERVER_NAME: AnyHttpUrl = os.getenv("PROTOCOL") + os.getenv("SERVER_NAME") + os.getenv("BASE_PATH")
+    COMPLETE_SERVER_NAME: AnyHttpUrl = os.getenv(
+        "PROTOCOL") + os.getenv("SERVER_NAME") + os.getenv("BASE_PATH")
     API_V1_STR: str = "/api/v1"
-    
-    PRODUCTION_MODE : bool =  "https" in os.getenv("PROTOCOL")
-    PROJECT_NAME : str = "Auth API"
+
+    PRODUCTION_MODE: bool = "https" in os.getenv("PROTOCOL")
+    PROJECT_NAME: str = "Auth API"
+
 
 settings = Settings()
